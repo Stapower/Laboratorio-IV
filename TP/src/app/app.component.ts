266a79e93b6c0ecc3e5e45fe113f9c556c6ccd0a
Sebastian Tomas Argañaras <tomynfs2@gmail.com>
2017-06-27 01:08:33 -0300

    Add files via upload

--- TP/src/app/app.component.ts
+++ TP/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from './_services/index';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  model: any = {};
+  loading = false;
+  error = '';
+  showLogginModel=true;
+
+      constructor(
+        private authenticationService: AuthenticationService) { }
+    ngOnInit() {
+        // reset login status
+        this.authenticationService.logout();
+    }
+    login() {
+        this.loading = true;
+        this.authenticationService.login(this.model.username, this.model.password)
+            .subscribe(result => {
+                if (result === true) {
+                      this.showLogginModel=false;
+                }
+                else {
+                    this.error = 'Username or password is incorrect';
+                    this.loading = false;
+                }
+            });
+    }
 }
--- TP/src/app/app.module.ts
+++ TP/src/app/app.module.ts
@@ -5,14 +5,31 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+
+
+const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: '', component: AppComponent},
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: 'login' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
